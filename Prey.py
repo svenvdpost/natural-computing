bb9753cb4306cee2f5a61ac271bd2cbac58a2c0c
Sven <svenvdp@hotmail.nl>
2023-05-10 13:47:42 +0200

    creation of boid classes and first simulation with pygame

---
+++ Prey.py
@@ -0,0 +1,134 @@
+# Working version
+import numpy as np
+
+class Boids:
+    def __init__(self, 
+                 num_boids = 100, 
+                 width = 1000, 
+                 height = 1000, 
+                 alignment_distance = 50, 
+                 cohesion_distance = 100, 
+                 separation_distance = 25, 
+                 vision_distance = None,
+                 alignment_strength = 0.1, 
+                 cohesion_strength = 0.001, 
+                 separation_strength = 0.5, 
+                 noise_strength = 0.2,
+                 max_velocity = 5):
+      
+        self.num_boids = num_boids
+        #self.positions = np.random.uniform(low=[-width/2, -height/2], high=[width/2, height/2], size=(num_boids, 2))
+        self.positions = np.random.uniform(low=[0,0], high=[width, height], size=(num_boids, 2))        
+        #self.velocities = np.random.uniform(low=[-width/2, -height/2], high=[width/2, height/2], size=(num_boids, 2))
+        self.velocities = np.random.uniform(low=[0,0], high=[width, height], size=(num_boids, 2))
+        self.width = width
+        self.height = height
+
+        self.alignment_distance = alignment_distance 
+        self.cohesion_distance = cohesion_distance 
+        self.separation_distance = separation_distance 
+        self.alignment_strength = alignment_strength 
+        self.cohesion_strength = cohesion_strength 
+        self.separation_strength = separation_strength 
+        self.noise_strength = noise_strength
+        
+        if vision_distance != None:
+            self.alignment_distance = vision_distance
+            self.cohesion_distance = vision_distance
+            self.separation_distance = vision_distance
+            self.alignment_strength = vision_distance
+            self.cohesion_strength = vision_distance
+            self.separation_strength = vision_distance
+
+
+        self.max_velocity = max_velocity 
+        self.positions_over_time = [self.positions]
+        self.velocities_over_time = [self.velocities]
+
+        # Optimization        
+        self.distances = sorted([("alignment", self.alignment_distance), ("cohesion", self.cohesion_distance), ("separation", self.separation_distance)], key=lambda x: x[1])
+
+    def step(self):
+
+        distances = self.get_distances()
+
+        alignment = self.alignment_rule(distances)
+        cohesion = self.cohesion_rule(distances)
+        separation = self.separation_rule(distances)
+
+        alignment_correction  = (alignment + np.random.uniform(-1,1, (self.num_boids, 2))   * self.noise_strength) * self.alignment_strength
+        cohesion_correction   = (cohesion + np.random.uniform(-1,1, (self.num_boids, 2))    * self.noise_strength) * self.cohesion_strength
+        separation_correction = (separation + np.random.uniform(-1,1, (self.num_boids, 2)) * self.noise_strength) * self.separation_strength
+
+        self.velocities += alignment_correction + cohesion_correction + separation_correction
+        self.limit_velocity()
+        self.positions = self.wrap(self.positions + self.velocities)
+        self.positions_over_time.append(self.positions.copy())
+        self.velocities_over_time.append(self.velocities.copy())
+    
+    def step_no_save(self):
+        distances = self.get_distances()
+
+        alignment = self.alignment_rule(distances)
+        cohesion = self.cohesion_rule(distances)
+        separation = self.separation_rule(distances)
+
+        alignment_correction  = (alignment + np.random.uniform(-1,1, (self.num_boids, 2))   * self.noise_strength) * self.alignment_strength
+        cohesion_correction   = (cohesion + np.random.uniform(-1,1, (self.num_boids, 2))    * self.noise_strength) * self.cohesion_strength
+        separation_correction = (separation + np.random.uniform(-1,1, (self.num_boids, 2)) * self.noise_strength) * self.separation_strength
+
+        self.velocities += alignment_correction + cohesion_correction + separation_correction
+        self.limit_velocity()
+        self.positions = self.wrap(self.positions + self.velocities)
+
+        return self.positions
+
+    def get_distances(self):
+        return np.sqrt(np.sum((self.positions[:, np.newaxis] - self.positions) ** 2, axis=2))
+
+    def get_close_boids(self, rule_distance, distances):
+        return (distances < rule_distance) & (distances > 0)
+
+    def alignment_rule(self, distances):
+        close_boids = self.get_close_boids(self.alignment_distance, distances)
+        alignment = np.zeros((len(self.positions), 2))
+        for i in range(len(self.positions)):
+            neighbors = close_boids[i]
+            if any(neighbors):
+                alignment[i] = np.mean(self.velocities[neighbors], axis=0)
+        return alignment 
+
+    def cohesion_rule(self, distances):
+        close_boids = self.get_close_boids(self.cohesion_distance, distances)
+        cohesion = np.zeros((len(self.positions), 2))
+        for i in range(len(self.positions)):
+            neighbors = close_boids[i]
+            if any(neighbors):
+                cohesion[i] = np.mean(self.positions[neighbors], axis=0) - self.positions[i]
+        return cohesion 
+
+    def separation_rule(self, distances):
+        close_boids = self.get_close_boids(self.separation_distance, distances)
+        separation = np.zeros((len(self.positions), 2))
+        for i in range(len(self.positions)):
+            neighbors = close_boids[i]
+            if any(neighbors):
+                separation[i] = np.sum(self.positions[neighbors] - self.positions[i], axis=0)
+        return - separation 
+
+    def limit_velocity(self):
+        speed = np.sqrt(np.sum(self.velocities ** 2, axis=1))
+        too_fast = speed > self.max_velocity
+        self.velocities[too_fast] = self.velocities[too_fast] / speed[too_fast, np.newaxis] * self.max_velocity
+
+    def wrap(self, positions):
+        positions[:, 0] = np.where(positions[:, 0] > self.width, positions[:, 0] - self.width, positions[:, 0])
+        positions[:, 0] = np.where(positions[:, 0] < 0, positions[:, 0] + self.width, positions[:, 0])
+        positions[:, 1] = np.where(positions[:, 1] > self.height, positions[:, 1] - self.height, positions[:, 1])
+        positions[:, 1] = np.where(positions[:, 1] < 0, positions[:, 1] + self.height, positions[:, 1])
+        return positions
+
+    def run_simulation(self, num_steps):
+        for i in range(num_steps):
+            self.step()
+        return self.positions_over_time, self.velocities_over_time
---
+++ Simulation.py
@@ -0,0 +1,102 @@
+import pygame
+import time
+
+import Prey
+import Predator
+
+
+class Simulation:
+
+
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+
+        self.boids = self.init_boids()
+        self.canvas = self.init_pygame()
+
+    def init_boids(self):
+        # Define model parameters
+        num_boids = 100
+        alignment_distance = 50
+        cohesion_distance = 100
+        separation_distance = 25 #25
+        vision_distance = None
+        alignment_strength = 0.1
+        cohesion_strength = 0.001
+        separation_strength = 0.05
+        noise_strength = 0.1
+        max_velocity = 5    
+
+        # Create Boids object
+        boids = Prey.Boids(num_boids = num_boids, width = width, height = height, \
+            alignment_distance = alignment_distance, cohesion_distance = cohesion_distance, \
+            separation_distance = separation_distance, alignment_strength = alignment_strength, \
+            cohesion_strength = cohesion_strength, separation_strength = separation_strength, \
+            max_velocity = max_velocity)
+        
+        return boids
+
+    def init_pygame(self):
+        pygame.init()
+        canvas = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Boid simulation")
+
+        return canvas
+
+
+    def render_and_run(self, steps):
+
+        positions, velocities = self.boids.run_simulation(steps)
+        exit = False
+
+        while not exit:
+
+            for t in positions:
+
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        exit = True
+                        
+                if exit == True:
+                    break
+
+                self.canvas.fill((255,255,255))
+                for pos in t:
+                    pygame.draw.circle(self.canvas, (255,0,0), pos, 3)
+
+                pygame.display.update()
+
+                time.sleep(0.05)
+  
+    def run_forever(self):
+
+            exit = False
+
+            while not exit:
+                positions = self.boids.step_no_save()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        exit = True
+                            
+
+                self.canvas.fill((255,255,255))
+                for pos in positions:
+                    pygame.draw.circle(self.canvas, (255,0,0), pos, 3)
+
+                pygame.display.update()
+
+                time.sleep(0.05)
+
+
+if __name__ == "__main__":
+
+    # Define the simulation parameters
+    width = 700
+    height = 500
+    num_steps = 100    
+
+    simulation = Simulation(width, height)
+
+    #simulation.render_and_run(num_steps)   
+    simulation.run_forever()
