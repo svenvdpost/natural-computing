12593726225ac7a14f26c8b8f065c30f54f4155a
Sven <svenvdp@hotmail.nl>
2023-05-24 17:37:56 +0200

    made movement forces boid dependand and first draft of genetic code is in place

--- Boids.py
+++ Boids.py
@@ -23,13 +23,18 @@ class Boids:
         self.width = width
         self.height = height
 
-        self.alignment_distance = alignment_distance 
-        self.cohesion_distance = cohesion_distance 
-        self.separation_distance = separation_distance 
-        self.alignment_strength = alignment_strength 
-        self.cohesion_strength = cohesion_strength 
-        self.separation_strength = separation_strength 
-        self.noise_strength = noise_strength
+
+        # TODO suggestion: instead of drawing different samples for the x and y strenght, make them the same. Maybe not necessary however.
+
+        scale = 0.001
+
+        self.alignment_distance = np.random.normal(alignment_distance, scale, num_boids) # alignment_distance 
+        self.cohesion_distance = np.random.normal(cohesion_distance, scale, num_boids) # cohesion_distance 
+        self.separation_distance = np.random.normal(separation_distance, scale, num_boids) #  separation_distance 
+        self.alignment_strength = np.random.normal(alignment_strength, scale, (num_boids, 2)) # alignment_strength 
+        self.cohesion_strength = np.random.normal(cohesion_strength, scale, (num_boids, 2)) # cohesion_strength 
+        self.separation_strength = np.random.normal(separation_strength, scale, (num_boids, 2)) # separation_strength 
+        self.noise_strength = np.random.normal(noise_strength, scale, (num_boids, 2)) # noise_strength
         
         if vision_distance != None:
             self.alignment_distance = vision_distance
@@ -40,49 +45,49 @@ class Boids:
             self.separation_strength = vision_distance
 
 
-        self.max_velocity = max_velocity 
+        self.max_velocity = np.random.normal(max_velocity, scale, num_boids) # max_velocity 
         self.positions_over_time = [self.positions]
         self.velocities_over_time = [self.velocities]
 
         # Optimization        
-        self.distances = sorted([("alignment", self.alignment_distance), ("cohesion", self.cohesion_distance), ("separation", self.separation_distance)], key=lambda x: x[1])
+        #self.distances = sorted([("alignment", self.alignment_distance), ("cohesion", self.cohesion_distance), ("separation", self.separation_distance)], key=lambda x: x[1])
 
         self.traits = {}
 
-    def step(self):
+    # def step(self):
 
-        distances = self.get_distances()
+    #     distances = self.get_distances()
 
-        alignment = self.alignment_rule(distances)
-        cohesion = self.cohesion_rule(distances)
-        separation = self.separation_rule(distances)
+    #     alignment = self.alignment_rule(distances)
+    #     cohesion = self.cohesion_rule(distances)
+    #     separation = self.separation_rule(distances)
 
-        alignment_correction  = (alignment + np.random.uniform(-1,1, (self.num_boids, 2))   * self.noise_strength) * self.alignment_strength
-        cohesion_correction   = (cohesion + np.random.uniform(-1,1, (self.num_boids, 2))    * self.noise_strength) * self.cohesion_strength
-        separation_correction = (separation + np.random.uniform(-1,1, (self.num_boids, 2)) * self.noise_strength) * self.separation_strength
+    #     alignment_correction  = (alignment + np.random.uniform(-1,1, (self.num_boids, 2))   * self.noise_strength) * self.alignment_strength
+    #     cohesion_correction   = (cohesion + np.random.uniform(-1,1, (self.num_boids, 2))    * self.noise_strength) * self.cohesion_strength
+    #     separation_correction = (separation + np.random.uniform(-1,1, (self.num_boids, 2)) * self.noise_strength) * self.separation_strength
 
-        self.velocities += alignment_correction + cohesion_correction + separation_correction
-        self.limit_velocity()
-        self.positions = self.wrap(self.positions + self.velocities)
-        self.positions_over_time.append(self.positions.copy())
-        self.velocities_over_time.append(self.velocities.copy())
+    #     self.velocities += alignment_correction + cohesion_correction + separation_correction
+    #     self.limit_velocity()
+    #     self.positions = self.wrap(self.positions + self.velocities)
+    #     self.positions_over_time.append(self.positions.copy())
+    #     self.velocities_over_time.append(self.velocities.copy())
     
-    def step_pygame(self):
-        distances = self.get_distances()
+    # def step_pygame(self):
+    #     distances = self.get_distances()
 
-        alignment = self.alignment_rule(distances)
-        cohesion = self.cohesion_rule(distances)
-        separation = self.separation_rule(distances)
+    #     alignment = self.alignment_rule(distances)
+    #     cohesion = self.cohesion_rule(distances)
+    #     separation = self.separation_rule(distances)
 
-        alignment_correction  = (alignment + np.random.uniform(-1,1, (self.num_boids, 2))   * self.noise_strength) * self.alignment_strength
-        cohesion_correction   = (cohesion + np.random.uniform(-1,1, (self.num_boids, 2))    * self.noise_strength) * self.cohesion_strength
-        separation_correction = (separation + np.random.uniform(-1,1, (self.num_boids, 2)) * self.noise_strength) * self.separation_strength
+    #     alignment_correction  = (alignment + np.random.uniform(-1,1, (self.num_boids, 2))   * self.noise_strength) * self.alignment_strength
+    #     cohesion_correction   = (cohesion + np.random.uniform(-1,1, (self.num_boids, 2))    * self.noise_strength) * self.cohesion_strength
+    #     separation_correction = (separation + np.random.uniform(-1,1, (self.num_boids, 2)) * self.noise_strength) * self.separation_strength
 
-        self.velocities += alignment_correction + cohesion_correction + separation_correction
-        self.limit_velocity()
-        self.positions = self.wrap(self.positions + self.velocities)
+    #     self.velocities += alignment_correction + cohesion_correction + separation_correction
+    #     self.limit_velocity()
+    #     self.positions = self.wrap(self.positions + self.velocities)
 
-        return self.positions, self.velocities
+    #     return self.positions, self.velocities
 
     def get_distances(self):
         return np.sqrt(np.sum((self.positions[:, np.newaxis] - self.positions) ** 2, axis=2))
@@ -120,7 +125,7 @@ class Boids:
     def limit_velocity(self):
         speed = np.sqrt(np.sum(self.velocities ** 2, axis=1))
         too_fast = speed > self.max_velocity
-        self.velocities[too_fast] = self.velocities[too_fast] / speed[too_fast, np.newaxis] * self.max_velocity
+        self.velocities[too_fast] = self.velocities[too_fast] / speed[too_fast, np.newaxis] * self.max_velocity[too_fast, np.newaxis]
 
     def wrap(self, positions):
         positions[:, 0] = np.where(positions[:, 0] > self.width, positions[:, 0] - self.width, positions[:, 0])
--- Genetic.py
+++ Genetic.py
@@ -1,24 +1,46 @@
 import Prey
 import Predator
+import Boids
 
 from random import shuffle
+import numpy as np
+from math import ceil
 
 class Genetic:
 
-    def __init__(self, mutation_rate) -> None:
+    def __init__(self, simulation, mutation_rate) -> None:
+        self.simulation = simulation
         self.mutation_rate = mutation_rate
 
     # mutate the stats of the boids proportional to the mutation_rate
-    def mutation(self, boid):
-        pass
+    def mutation(self, child):
+        mutate = np.random.choice([True,False], p=[self.mutation_rate, 1-self.mutation_rate])
+        if mutate:
+            pass
 
     # crossover the stats of the two parents
-    def crossover(self, parent1, parent2):
-        pass
+    def crossover(self, parent1, parent2, boidclass : Boids.Boids):
+        position = (boidclass.positions[parent1] + boidclass.positions[parent2]) / 2
+        velocity = (boidclass.velocities[parent1] + boidclass.velocities[parent2]) / 2
+        traits = {}
+
+        for trait, traitlist in boidclass.traits.items():
+            traits[trait] = (traitlist[parent1] + traitlist[parent2]) / 2
+
+        return (position, velocity, traits)
+            
 
     # create children from the two parents
-    def make_children(self, parent1, parent2):
-        pass
+    def make_children(self, parent1, parent2, boidclass : Boids.Boids):
+        num_children = ceil(np.random.normal(2))
+        children = []
+
+        for i in range(num_children):
+            child = self.crossover(parent1, parent2, boidclass)
+            self.mutation(child)
+            children.append(child)
+        
+        return children
 
     # randomly pair parents from the population
     def pair_random(self, population):
@@ -29,10 +51,10 @@ class Genetic:
         return zip(group1, group2)
 
     # create the next generation from the population
-    def next_generation(self, population):
+    def next_generation(self, population, boidclass):
         velocities = []
         positions  = []
         traits = {}
 
         for parent1, parent2 in self.pair_random(population):
-            children = self.make_children(parent1, parent2)
+            children = self.make_children(parent1, parent2, boidclass)
